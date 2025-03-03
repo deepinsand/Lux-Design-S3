725a5a1409984772b9b0fe4c8651ad614a310c37
Sandeep Jain <deepinsand@gmail.com>
2025-03-03 15:24:14 +0000

    actually randomized params

--- src/luxai_s3/params.py
+++ src/luxai_s3/params.py
@@ -90,35 +90,35 @@ class EnvParams:
 
     # option to change sap configurations
 
-env_params_ranges = dict(
-    # map_type=[1],
-    unit_move_cost=list(range(1, 2)),
-    unit_sensor_range=[2],
-    nebula_tile_vision_reduction=list(range(1, 2)),
-    nebula_tile_energy_reduction=[1],
-    unit_sap_cost=list(range(30, 31)),
-    unit_sap_range=list(range(4, 5)),
-    unit_sap_dropoff_factor=[0.25],
-    unit_energy_void_factor=[0.0625],
-    # map randomizations
-    nebula_tile_drift_speed=[-0.025],
-    energy_node_drift_speed=[0.01],
-    energy_node_drift_magnitude=list(range(3, 4)),
-)
-
-
 # env_params_ranges = dict(
 #     # map_type=[1],
-#     unit_move_cost=list(range(1, 6)),
-#     unit_sensor_range=[1, 2, 3, 4],
-#     nebula_tile_vision_reduction=list(range(0, 8)),
-#     nebula_tile_energy_reduction=[0, 1, 2, 3, 5, 25],
-#     unit_sap_cost=list(range(30, 51)),
-#     unit_sap_range=list(range(3, 8)),
-#     unit_sap_dropoff_factor=[0.25, 0.5, 1],
-#     unit_energy_void_factor=[0.0625, 0.125, 0.25, 0.375],
+#     unit_move_cost=list(range(1, 2)),
+#     unit_sensor_range=[2],
+#     nebula_tile_vision_reduction=list(range(1, 2)),
+#     nebula_tile_energy_reduction=[1],
+#     unit_sap_cost=list(range(30, 31)),
+#     unit_sap_range=list(range(4, 5)),
+#     unit_sap_dropoff_factor=[0.25],
+#     unit_energy_void_factor=[0.0625],
 #     # map randomizations
-#     nebula_tile_drift_speed=[-0.15, -0.1, -0.05, -0.025, 0.025, 0.05, 0.1, 0.15],
-#     energy_node_drift_speed=[0.01, 0.02, 0.03, 0.04, 0.05],
-#     energy_node_drift_magnitude=list(range(3, 6)),
+#     nebula_tile_drift_speed=[-0.025],
+#     energy_node_drift_speed=[0.01],
+#     energy_node_drift_magnitude=list(range(3, 4)),
 # )
+
+
+env_params_ranges = dict(
+    # map_type=[1],
+    unit_move_cost=list(range(1, 6)),
+    unit_sensor_range=[1, 2, 3, 4],
+    nebula_tile_vision_reduction=list(range(0, 8)),
+    nebula_tile_energy_reduction=[0, 1, 2, 3, 5, 25],
+    unit_sap_cost=list(range(30, 51)),
+    unit_sap_range=list(range(3, 8)),
+    unit_sap_dropoff_factor=[0.25, 0.5, 1],
+    unit_energy_void_factor=[0.0625, 0.125, 0.25, 0.375],
+    # map randomizations
+    nebula_tile_drift_speed=[-0.15, -0.1, -0.05, -0.025, 0.025, 0.05, 0.1, 0.15],
+    energy_node_drift_speed=[0.01, 0.02, 0.03, 0.04, 0.05],
+    energy_node_drift_magnitude=list(range(3, 6)),
+)
